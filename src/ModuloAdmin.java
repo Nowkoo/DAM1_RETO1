c16b588c3cc7178c7dc8473f383c6ae523698d8c
Andres <aluque0934@ieselcaminas.org>
2023-12-15 19:17:54 +0100

    Clase moduloAdmin

---
+++ src/ModuloAdmin.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class ModuloAdmin {
+    private ArrayList usuarios;
+    private ArrayList peticiones;
+    private ArrayList categoria;
+
+    private Usuario usuarioActual;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void loginAdmin() {
+
+    }
+
+    public static void consultarPeticiones() {
+
+    }
+
+    public static void consultarTickets() {
+
+    }
+
+    public static void generarTicket() {
+
+    }
+
+    public static void modificarTicket() {
+
+    }
+
+    public static void modificarCategoriaPeticion() {
+
+    }
+
+    public static void filtrarTicketPorTiempo() {
+
+    }
+
+    public static void filtrarTicketPorInventario() {
+
+    }
+
+    public static void cargarDatosUsuario() {
+
+    }
+
+    public static void cargarDatosPeticiones() {
+
+    }
+
+    public static void cargarDatosCategoria() {
+
+    }
+
+    public static void cargarDatosTecnicos() {
+
+    }
+
+    public static void cargarDatosAdmins() {
+
+    }
+
+    public static void cargarDatosTickets() {
+
+    }
+
+    public static void guardarDatosPeticiones() {
+
+    }
+
+    public static void guardarDatosTickets() {
+
+    }
+}
