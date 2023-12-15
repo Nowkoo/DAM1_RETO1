738b76b0b505f2578264861ab7d3df78394ba43b
Andres <aluque0934@ieselcaminas.org>
2023-12-15 18:51:18 +0100

    Clase Admin

---
+++ src/Admin.java
@@ -0,0 +1,39 @@
+public class Admin {
+    private int id;
+
+    private String nombre;
+
+    private String password;
+
+    Admin(int id, String nombre, String password) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
